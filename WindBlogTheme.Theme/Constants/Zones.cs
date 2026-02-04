9533d0daca280612d48e201ac854fa35ec03882d
Miloš Jovanović <milos.jovanovic@lombiq.com>
2026-02-04 03:53:56 +0100

    Working on the contact page; styling form elements; adding page sidebars.

---
+++ WindBlogTheme.Theme/Constants/Zones.cs
@@ -0,0 +1,9 @@
+﻿namespace WindBlogTheme.Theme.Constants;
+
+public static class Zones
+{
+    public const string BlogPostArticleBelowContent = nameof(BlogPostArticleBelowContent);
+    public const string PageSidebar = nameof(PageSidebar);
+    public const string SpecificPageSidebar = nameof(SpecificPageSidebar);
+}
+
